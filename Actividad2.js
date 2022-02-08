b779c07a8145ce0dc4c4f5ca119788acfe439333
knfolgar <99238288+knfolgar@users.noreply.github.com>
2022-02-07 23:42:09 -0600

    Add files via upload

---
+++ Actividad2.js
@@ -0,0 +1,29 @@
+// Declaracion de variable mes con valor por defecto
+var mes = 10;
+
+//Ejecución de sentencia anidada para la comprobación de la variable mes
+if (mes == 12) {
+
+	alert("es invierno")
+
+} else if (mes == 1 || mes <= 2) {
+
+	alert("es invierno")
+
+} else if (mes == 3 || mes <= 5) {
+
+	alert("es primavera")
+
+} else if (mes == 6 || mes <= 8) {
+
+	alert("es verano")
+
+} else if( mes == 9 || mes <= 11) {
+
+	alert("es otoño")
+}
+
+//Impresión en pantalla de carné y nombre
+alert("22006896 - Kleyver Folgar");
+
+
